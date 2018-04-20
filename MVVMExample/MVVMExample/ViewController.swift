77113bd2d91ff71fb5821244cc956a9a40e477cc
geoninh <ninhnm91@gmail.com>
2018-04-20 16:40:42 +0700

    add source code

---
+++ MVVMExample/MVVMExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by NinhNM_ACV on 4/20/18.
+//  Copyright © 2018 JuniorStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
